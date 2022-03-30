f6cfe21e78faaf4f56a3fa0ea9ba666b7994d7f7
DerekAlvarado <81263819+Derek069@users.noreply.github.com>
2022-03-30 16:17:14 -0600

    Added the request to our own API

--- src/lib/mashupAPIRequest.js
+++ src/lib/mashupAPIRequest.js
@@ -1,2 +1,24 @@
-//Request a la API propia
-//Luis Villa part of making request to the API
+const URL = "https://mashup-series-api.herokuapp.com/"
+
+class http{
+    static instance = new http()
+getSeries = async() => {
+    try{
+        const request = await fetch (`${URL}series/`)
+        const response = await request.json()
+        return response
+    }catch(err){
+        throw new Error(err);
+    }
+ 
+}
+getSerie = async(id) => {
+    try{
+        const request = await fetch (`${URL}serie/${id}/`)
+        const response = await request.json()
+        return response
+    }catch(err){
+        throw new Error(err);
+    }
+}
+}export default http;
