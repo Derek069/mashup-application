948ff11c46312736bd90c1631706cb3ec0464c7b
DerekAlvarado <81263819+Derek069@users.noreply.github.com>
2022-03-30 16:15:17 -0600

    Added the main page for the list of series

---
+++ src/pages/seriesHome.js
@@ -0,0 +1,32 @@
+import React from "react";
+import http from "../lib/request";
+import Serie from "../components/Serie";
+
+class SeriesHome extends React.Component{
+    state={
+        series:[]
+    }
+    componentDidMount = () =>{
+        this.getData(URL)
+    }
+    getData = async(api_url) =>{
+        const response = await http.instance.getSeries()
+        this.setState({series: response.results})
+        console.log(this.state.series)
+    }
+    render(){
+        return(
+            <React.Fragment>
+                 <body>
+                     <h1>Marvel's Comic Series</h1>
+                     
+                     <div> 
+                        {this.state.series.map((serie) => 
+                        {return <Serie serie = {serie}/>
+                        })}
+                        </div> 
+                 </body>
+            </React.Fragment> 
+        )
+    }
+}export default SeriesHome;
